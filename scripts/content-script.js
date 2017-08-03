260784b40a66d7e8a645a2a0f079af16781fa951
Martin Giger <martin@humanoids.be>
2017-08-03 23:32:37 +0200

    Reduce abuse surface

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,14 +1,17 @@
-/* global XPCNativeWrapper, exportFunction */
-/* eslint-disable new-cap */
+/* eslint-disable no-eval */
 const EVENT = "we-ns-notificationshown";
 window.addEventListener(EVENT, ({ detail }) => {
-    if(!detail) {
+    if(!detail && Notification.permission === "granted") {
         browser.runtime.sendMessage("new-notification");
     }
 }, {
     passive: true,
     capture: true
 });
+
+// I tried doing this the XRay wrappers way, but there's just so much that can
+// (and does) go wrong, that I eventually gave up and this works very well and
+// seems secure-enough.
 
 window.eval(`window.Notification = class extends Notification {
     constructor(title, options) {
