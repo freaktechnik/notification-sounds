8f5c151f59f8e17d4b61aa78aec9115406e27c61
Martin Giger <martin@humanoids.be>
2017-11-18 00:32:03 +0100

    fix: Instanceof operator on Notification instances and undefined property on options

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -24,23 +24,33 @@ const OPTIONS_INDEX = 1,
      * @returns {Notification} Instance.
      */
     ModifiedNotification = function(...args) {
-        dispatchNotificationEvent(args[OPTIONS_INDEX]);
+        dispatchNotificationEvent(args.length > OPTIONS_INDEX ? args[OPTIONS_INDEX] : undefined);
         return new OriginalNotification(...args);
     },
-    descriptor = Object.getOwnPropertyDescriptors(OriginalNotification);
+    descriptor = window.wrappedJSObject.Object.getOwnPropertyDescriptors(OriginalNotification);
 
-delete descriptor.prototype;
 
 // Replace original Notification constructor with the version that plays sounds.
 window.wrappedJSObject.Notification = exportFunction(ModifiedNotification, window, {
     allowCrossOriginArguments: true
 });
-// Ensure the prototype is correct, inheriting from the original prototype
-window.wrappedJSObject.Notification.prototype = cloneInto({}, window);
+// Ensure the prototype is correct, inheriting from the original prototype.
+descriptor.prototype.value = cloneInto({}, window);
+Object.setPrototypeOf(descriptor.prototype.value, OriginalNotification.prototype);
+// Make instanceof work with overwritten constructor.
+descriptor[Symbol.hasInstance] = cloneInto({
+    enumerable: false,
+    writable: false,
+    configurable: false,
+    value: (instance) => instance instanceof OriginalNotification
+}, window, {
+    cloneFunctions: true
+});
+// Set static propertties on our constructor.
+window.wrappedJSObject.Object.defineProperties(window.wrappedJSObject.Notification, descriptor);
+// Set the constructor property. Have to set it here so it gets set to the same instance as the main constructor.
 window.wrappedJSObject.Notification.prototype.constructor = window.wrappedJSObject.Notification;
-Object.setPrototypeOf(window.wrappedJSObject.Notification.prototype, OriginalNotification.prototype);
-// Set static propertties on our constructor
-Object.defineProperties(window.wrappedJSObject.Notification, descriptor);
+// Set the top level proto, which for some reason is an EventTarget.
 Object.setPrototypeOf(window.wrappedJSObject.Notification, Object.getPrototypeOf(OriginalNotification));
 
 // Override serviceWorker notifications in website scope.
@@ -48,10 +58,10 @@ const original = window.wrappedJSObject.ServiceWorkerRegistration.prototype.show
     /**
      * @param {?} args - Arguments.
      * @this {ServiceWorkerRegistration}
-     * @returns {Promise}
+     * @returns {Promise} Resolves with the original promise.
      */
     replacement = function(...args) {
-        dispatchNotificationEvent(args[OPTIONS_INDEX]);
+        dispatchNotificationEvent(args.length > OPTIONS_INDEX ? args[OPTIONS_INDEX] : undefined);
         return original.call(this, ...args);
     };
 
